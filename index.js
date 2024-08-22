295d5f6d24b679a746fff34cb4de93f356da5038
GarciaMgc <114542432+GarciaMgc@users.noreply.github.com>
2024-08-22 17:51:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+// const, let ou var para declarar variavel
+// let numeroNumero = 1;
+// let numeroTexto = "1";
+// Um igual é atribuição, dois iguais é comparação e três iguais compara o tipo do dado
+
+// if (numeroNumero === numeroTexto){
+//     console.log("dei certo")
+// }
+// else{
+//     console.log("dei errado! :(")
+// }
+
+// &&, and, e
+// ||, or, ou
+
+// let numero = '321';
+
+// if(numero == 0 || numero === 321 ){
+//     console.log("dei certo")
+// }
+// else{
+//     console.log("dei errado")
+// }
+
+// if(numero == 0 && numero == '321' ){
+//     console.log("dei certo")
+// }
+
+// let numero = "Olá";
+
+// if(numero == 0){
+//     console.log("Sou zero");
+// }
+// else if(numero < 0){
+//     console.log("sou negativo!");
+// }
+// else if(numero > 0){
+//     console.log("sou positivo!")
+// }
+// else{
+//     console.log("Não sou um numero!")
+// }
+
+// Javascript e python são linguagens dinamicas
+// Java é uma linguagem fortemente tipada
+
+// let proanes = ["Sheila", "Herbert", "Leandra", "Marillia", 10, 30, ["olá", 10, 30]]
+// console.log(proanes[6][0]);
+
+// proanes[6][2]="estou adicionando";
+// console.log(proanes)
+// Metodo push() coloca no final
+
+// proanes.push("Galinha");
+// console.log(proanes)
+
+// // Metodo pop() tira do final
+
+// proanes.pop();
+// console.log(proanes)
+
+// Metodo unshift adiciona um elemento no inicio da lista
+
+// proanes.unshift("Bolsonaro")
+// console.log(proanes)
+
+// Metodo shift remove o primeiro elemento da lista
+
+// proanes.shift()
+// console.log(proanes)
+
+// Lenght mostra numero de elementos de uma lista 
+
+// let nomeLegal = proanes.length;
+// console.log(nomeLegal);
+
+// let palavra = "olá munde"
+// console.log(palavra.length - 1)
+
+// let proanes = ["Sheila", "Herbert", "Leandra", ["M","a","r","i"], 10, 30, ["e","r","t"]]
+// "", '' ou ``
+// console.log(proanes[proanes.length - 3][2])
+// console.log(proanes[proanes.length - 4][2])
+
+
+// cont = 1;
+
+// while(cont <= 11){
+    
+  
+//     console.log(cont)
+//     if(cont == 8){
+//         console.log("finalizando...")
+//         break;
+//     }
+//     cont++;
+// }
+
